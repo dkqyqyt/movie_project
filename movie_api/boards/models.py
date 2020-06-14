b0bd5c699aec29b4efe4bbdd30b8b362b06c7305
dkqyqyt <rsm4282@naver.com>
2020-06-14 16:58:18 +0900

    Add rating range 1 to 10

--- movie_api/boards/models.py
+++ movie_api/boards/models.py
@@ -2,9 +2,18 @@ from django.db import models
 from django.conf import settings
 from movies.models import Movie
 
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    rating = models.IntegerField()
+    rating = IntegerRangeField(max_value=10, min_value=1)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
