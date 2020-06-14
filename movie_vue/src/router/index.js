b1788870c757a7500b0186a57a1d0e7c64736dc2
dkqyqyt <rsm4282@naver.com>
2020-06-14 16:06:46 +0900

    Add router navigation guards

--- movie_vue/src/router/index.js
+++ movie_vue/src/router/index.js
@@ -101,7 +101,7 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to,from,next) => {
-  const publicPages = ['Login', 'Signup', 'MovieList', 'MovieDetail', 'ArticleList', 'Home']
+  const publicPages = ['Login', 'Signup', 'MovieList', 'MovieDetail', 'ArticleList', 'ArticleDetail', 'Home']
   const authPages = ['Login', 'Signup']
 
   const authRequired = !publicPages.includes(to.name)
