3cf7431cd031fe6c653a9f910a3760de1f317c7d
Daniel_Lee <fifa920@naver.com>
2020-06-15 18:09:18 +0900

    Make Home.vue, recommend page

--- movie_vue/src/views/Home.vue
+++ movie_vue/src/views/Home.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
       <h1>Home</h1>
+        <div class="ui very relaxed items">
+          <div class="item">
+            <div class="image">
+              <img src="'https://pixabay.com/illustrations/film-photo-slides-cinema-1668918/'">
+            </div>
+            <div class="content">
+              
+              <div class="description">
+                <p>Ut imperdiet dignissim feugiat. Phasellus tristique odio eu justo dapibus, nec rutrum ipsum luctus. Ut posuere nec tortor eu ullamcorper. <a href="#link">Etiam pellentesque</a> tincidunt tortor, non sagittis nibh pretium sit amet. Sed neque dolor, blandit eu ornare vel, lacinia porttitor nisi. Vestibulum sit amet diam rhoncus, consectetur enim sit amet, interdum mauris. Praesent feugiat finibus quam, porttitor varius est egestas id.</p>
+              </div>
+              <a class="ui blue button" href="#link">Link Header</a>
+            </div>
+          </div>
+          <div class="item">
+            <div class="image">
+              <img src="/images/wireframe/image.png">
+            </div>
+            <div class="content">
+              
+              <div class="description">
+                <p>Ut imperdiet dignissim feugiat. Phasellus tristique odio eu justo dapibus, nec rutrum ipsum luctus. Ut posuere nec tortor eu ullamcorper. <a href="#link">Etiam pellentesque</a> tincidunt tortor, non sagittis nibh pretium sit amet. Sed neque dolor, blandit eu ornare vel, lacinia porttitor nisi. Vestibulum sit amet diam rhoncus, consectetur enim sit amet, interdum mauris. Praesent feugiat finibus quam, porttitor varius est egestas id.</p>
+              </div>
+              <a class="ui blue button" href="#link">Link Header</a>
+            </div>
+          </div>
+        </div>
   </div>
 </template>
 
 <script>
 export default {
     name: 'Home'
+  
 }
 </script>
 
